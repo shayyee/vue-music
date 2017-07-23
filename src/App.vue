12083ee45c39c25312e0ffda5f340be61dcd7ef7
shayyee <shayyee@163.com>
2017-07-23 17:47:14 +0800

    4-3

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,20 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <m-header></m-header>
+    <tab></tab>
     <router-view></router-view>
   </div>
 </template>
 
-<script>
+<script type="text/ecmascript-6">
+  import MHeader from './components/m-header/m-header'
+  import Tab from 'components/tab/tab'
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    MHeader,
+    Tab,
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
