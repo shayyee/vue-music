a1bd3e48ba1c0b828c983f5e0c6dfb47321ff1b9
shayyee <xxy1024235202@163.com>
2018-04-26 17:19:46 +0800

    播放器内核1

--- src/store/state.js
+++ src/store/state.js
@@ -1,8 +1,16 @@
 /**
  * Created by lenovo on 2018/3/19.
  */
+import {playMode} from "common/js/config"
 const state = {
-  singer: {}
+  singer: {},
+  playing: false,    // play or pause
+  fullScreen: false, // 播放器展开和收起
+  playList: [],      // 播放列表
+  sequenceList: [],  //支持各种播放模式
+  mode: playMode.sequence, // 播放模式
+  currentIndex: -1,        // 当前播放的歌曲索引
+
 }
 
 export default state;
