ed3057f2e341f9c2b4979c6604cafa4b35834e4b
shayyee <xxy1024235202@163.com>
2018-07-25 17:32:56 +0800

    显示搜索历史

--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -3,29 +3,31 @@
         <div class="search-box-wrapper">
             <search-box ref="searchBox" @query="onQueryChange"></search-box>
         </div>
-        <div class="shortcut-wrapper" v-show="!query">
-            <div class="shortcut">
-                <div class="hot-key">
-                    <h1 class="title">热门搜索</h1>
-                    <ul>
-                        <li @click="addQuery(item.k)" class="item" v-for="item in hotKey">
-                            <span>{{item.k}}</span>
-                        </li>
-                    </ul>
-                </div>
-                <div class="search-history" v-show="searchHistory.length">
-                    <h1 class="title">
-                        <span class="text">搜索历史</span>
-                        <span class="clear">
+        <div ref="shortcutWrapper" class="shortcut-wrapper" v-show="!query">
+            <scroll ref="shortcut" class="shortcut" :data="shortcut">
+                <div>
+                    <div class="hot-key">
+                        <h1 class="title">热门搜索</h1>
+                        <ul>
+                            <li @click="addQuery(item.k)" class="item" v-for="item in hotKey">
+                                <span>{{item.k}}</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="search-history" v-show="searchHistory.length">
+                        <h1 class="title">
+                            <span class="text">搜索历史</span>
+                            <span class="clear">
                             <i class="icon-clear"></i>
                         </span>
-                    </h1>
-                    <search-list :searches="searchHistory"></search-list>
+                        </h1>
+                        <search-list :searches="searchHistory"></search-list>
+                    </div>
                 </div>
-            </div>
+            </scroll>
         </div>
-        <div class="search-result" v-show="query">
-            <suggest @select="saveSearch" @listScroll="blurInput" :query="query"></suggest>
+        <div ref="searchResult" class="search-result" v-show="query">
+            <suggest ref="suggest" @select="saveSearch" @listScroll="blurInput" :query="query"></suggest>
         </div>
         <router-view></router-view>
     </div>
@@ -34,16 +36,20 @@
 <script type="text/ecmascript-6">
   import SearchBox from 'base/search-box/search-box'
   import SearchList from 'base/search-list/search-list'
+  import Scroll from 'base/scroll/scroll'
   import Suggest from 'components/suggest/suggest'
   import {getHotKey} from 'api/search'
   import {ERR_OK} from 'api/config'
   import {mapActions, mapGetters} from 'vuex'
+  import {playListMixin} from 'common/js/mixin'
 
   export default {
+    mixin: [playListMixin],
     components: {
       SearchBox,
       Suggest,
-      SearchList
+      SearchList,
+      Scroll
     },
     data() {
       return {
@@ -55,7 +61,10 @@
       this._getHotKey()
     },
     computed: {
-      ...mapGetters(['searchHistory'])
+      ...mapGetters(['searchHistory']),
+      shortcut() {
+        return this.hotKey.concat(this.searchHistory)
+      }
     },
     methods: {
       ...mapActions(['saveSearchHistory']),
@@ -65,6 +74,14 @@
             this.hotKey = res.data.hotkey.slice(0,10)
           }
         })
+      },
+      handlePlayList(playlist) {
+        const bottom = playlist.length > 0? '60px': ''
+        this.$refs.shortcutWrapper.style.bottom = bottom
+        this.$refs.shortcut.refresh()
+
+        this.$refs.searchResult.style.bottom = bottom
+        this.$refs.suggest.refresh()
       },
       addQuery(item) {
         this.$refs.searchBox.setQuery(item)
